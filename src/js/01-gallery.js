37b82000e0725dafd0dbf529d09e8b40ec12e7dd
Shchyrova Yelyzaveta <lissa02102005@gmail.com>
2023-04-30 11:10:21 +0200

    Add first example

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+
+const galleryItemsToStr = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"></a></li>`
+  )
+  .join("");
+
+galleryRef.innerHTML += galleryItemsToStr;
+
+
+galleryRef.addEventListener("click", onImageClick);
+
+function onImageClick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img width="900" height="550" src="${evt.target.dataset.source}">`,
+    {
+      closable: true,
+      onShow() {
+        window.addEventListener("keydown", onCloseModal);
+      },
+      onClose() {
+        window.removeEventListener("keydown", onCloseModal);
+      },
+    }
+  );
+  instance.show();
+
+  function onCloseModal(evt) {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  }
+}

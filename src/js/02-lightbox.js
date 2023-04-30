f2e551c0c7da0b19dd166ba7fbfb5167445bda4f
Shchyrova Yelyzaveta <lissa02102005@gmail.com>
2023-04-30 11:49:02 +0200

    Add second example

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+const galleryItemsToStr = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a></li>`
+  )
+  .join("");
+
+galleryRef.innerHTML += galleryItemsToStr;
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionSelector: "img",
+  captionsData: "alt",
+  captionDelay: 250,
+});
